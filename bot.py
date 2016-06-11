a0ae0828ee1078b96f596ca93f52046471e62acf
ironjanowar <ironjanowar@gmail.com>
2016-06-11 12:06:03 +0200

    Update added

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telebot
 import os.path as path
 import sys
+import json
 
 # Creamos el bot
 if not path.isfile("bot.token"):
@@ -10,13 +11,42 @@ if not path.isfile("bot.token"):
 with open("./bot.token", "r") as TOKEN:
     bot = telebot.TeleBot(TOKEN.readline().strip())
 
+if not path.isfile("./data/admins.json"):
+    with open('./data/admins.json', 'w') as adminData:
+        adminData.write('{}')
+        adminData.close
+
+with open('./data/admins.json', 'r') as adminData:
+    admins = json.load(adminData)
+
+# Funciones
+
+
+def isAdmin_fromPrivate(message):
+    if message.chat.type == 'private':
+        userID = message.from_user.id
+        if str(userID) in admins:
+            return True
+    return False
+
 # Handlers
 
 
 @bot.message_handler(content_types=['document'])
 def spam_pdf(message):
-    bot.forward_message("@openlibra_channel", message.chat.id, message.message_id, disable_notification=True)
-    bot.reply_to(message, "Mensaje reenviado a @openlibra_channel")
+    if message.document.file_name.endswith(".jpg"):
+        bot.forward_message("@openlibra_channel", message.chat.id, message.message_id, disable_notification=True)
+        bot.reply_to(message, "Mensaje reenviado a @openlibra_channel")
+
+
+@bot.message_handler(commands=['update'])
+def auto_update(message):
+    if isAdmin_fromPrivate(message):
+        bot.reply_to(message, "Reiniciando..\n\nPrueba algun comando en 10 segundos")
+        print("Updating..")
+        sys.exit()
+    else:
+        bot.reply_to(message, "Este comando es solo para admins y debe ser enviado por privado")
 
 # Correr bot
 print("Running...")
